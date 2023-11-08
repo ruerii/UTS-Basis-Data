06e5401cc1313924cc8cbfdd9b205fa0c5ceca32
ruerii <129568954+ruerii@users.noreply.github.com>
2023-11-09 00:01:07 +0700

    Jawaban1.sql

--- Jawaban1.sql
+++ Jawaban1.sql
@@ -1,21 +1,27 @@
 CREATE TABLE mahasiswa (
-	mahasiswaNumber INT PRIMARY KEY,
+	mahasiswaNumber INT auto_increment ,
 	mahasiswaNama VARCHAR(225),
 	jurusan VARCHAR (225),
 	kampus VARCHAR (225),
+	
+	PRIMARY KEY (mahasiswaNumber)
 	);
 	
 
 CREATE TABLE dosen (
-	dosenNumber INT PRIMARY KEY,
+	dosenNumber INT auto_increment,
 	dosenName VARCHAR(225),
 	kampus VARCHAR (225),
-	dosenMatakuliah VARCHAR (225)
+	dosenMatakuliah VARCHAR (225),
+	
+	PRIMARY KEY (dosenNumber)
 	);
 	
 CREATE TABLE mata_kuliah (
-	matakuliahNumber INT PRIMARY KEY,
+	matakuliahNumber INT auto_increment,
 	dosenNumber int (11),
 	matakuliahName VARCHAR (225),
-	sks VARCHAR (11)
+	sks VARCHAR (11),
+	
+	PRIMARY KEY (matakuliahNumber)
 	);
